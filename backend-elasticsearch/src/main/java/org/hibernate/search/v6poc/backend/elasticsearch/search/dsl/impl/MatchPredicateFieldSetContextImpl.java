abd47559aeb12e958ab0c80098898b66384c214a
Yoann Rodière <yoann@hibernate.org>
2017-12-12 14:23:35 +0100

    Add natural terminal calls to the .match() and .range() predicate building syntax
    ... so that a call to .end() is not necessary anymore.
    
    As discussed during the F2F meeting.

--- backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/search/dsl/impl/MatchPredicateFieldSetContextImpl.java
+++ backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/search/dsl/impl/MatchPredicateFieldSetContextImpl.java
@@ -17,7 +17,6 @@ import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.El
 import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.SearchPredicateBuilder;
 import org.hibernate.search.v6poc.search.dsl.predicate.MatchPredicateFieldSetContext;
-import org.hibernate.search.v6poc.search.dsl.predicate.MatchPredicateTerminalContext;
 
 import com.google.gson.JsonObject;
 
@@ -54,7 +53,7 @@ class MatchPredicateFieldSetContextImpl<N>
 	}
 
 	@Override
-	public MatchPredicateTerminalContext<N> matching(Object value) {
+	public N matching(Object value) {
 		return commonState.matching( value );
 	}
 
@@ -63,25 +62,19 @@ class MatchPredicateFieldSetContextImpl<N>
 		queryBuilders.stream().map( SearchPredicateBuilder::build ).forEach( collector );
 	}
 
-	public static class CommonState<N> extends MultiFieldPredicateCommonState<N, MatchPredicateFieldSetContextImpl<N>>
-			implements MatchPredicateTerminalContext<N> {
+	public static class CommonState<N> extends MultiFieldPredicateCommonState<N, MatchPredicateFieldSetContextImpl<N>> {
 
 		public CommonState(SearchTargetContext targetContext, Supplier<N> nextContextProvider) {
 			super( targetContext, nextContextProvider );
 		}
 
-		public MatchPredicateTerminalContext<N> matching(Object value) {
+		public N matching(Object value) {
 			getQueryBuilders().forEach( b -> b.value( value ) );
-			return this;
+			return getNextContextProvider().get();
 		}
 
 		private Stream<MatchPredicateBuilder> getQueryBuilders() {
 			return getFieldSetContexts().stream().flatMap( f -> f.queryBuilders.stream() );
-		}
-
-		@Override
-		public N end() {
-			return getNextContextProvider().get();
 		}
 
 	}
