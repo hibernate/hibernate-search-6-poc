5617e8d5db16b602d23eee5a8100759a01498a06
Yoann Rodière <yoann@hibernate.org>
2018-01-23 15:39:04 +0100

    Rename Bridge.contribute() to bind()

--- mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/building/impl/PojoIndexModelBinder.java
+++ mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/building/impl/PojoIndexModelBinder.java
@@ -24,7 +24,7 @@ import org.hibernate.search.v6poc.entity.pojo.processing.impl.ValueProcessor;
  * them.
  * <p>
  * Incidentally, this will also generate the index model,
- * due to bridges contributing to the index model as we contribute them.
+ * due to bridges contributing to the index model as we bind them.
  *
  * @author Yoann Rodiere
  */
