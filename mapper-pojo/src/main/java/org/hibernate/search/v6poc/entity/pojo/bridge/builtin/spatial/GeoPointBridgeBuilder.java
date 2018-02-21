f4fc630d2d2edb3893b70809c4ad1cc12caa3b8d
Yoann Rodière <yoann@hibernate.org>
2018-02-21 12:36:21 +0100

    Take the Store parameter into account in GeoPointBridge

--- mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/bridge/builtin/spatial/GeoPointBridgeBuilder.java
+++ mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/bridge/builtin/spatial/GeoPointBridgeBuilder.java
@@ -19,7 +19,7 @@ import org.hibernate.search.v6poc.entity.pojo.bridge.Bridge;
 public class GeoPointBridgeBuilder implements AnnotationBridgeBuilder<Bridge, GeoPointBridge> {
 
 	private String fieldName;
-	private Store store;
+	private Store store = Store.DEFAULT;
 	private String markerSet;
 
 	@Override
