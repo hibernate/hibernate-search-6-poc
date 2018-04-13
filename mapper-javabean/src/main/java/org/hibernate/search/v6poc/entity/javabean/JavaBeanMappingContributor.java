hibernate/hibernate-search-6-poc

commit 70aadce41b6d67f6934f05066f2eef0272ed6e06

Remove the implementation type leak from PojoMapperFactory
Add proper SPIs for the PojoMappingFactory in particular, and types
related to mapping creation in general.

mapper-javabean/src/main/java/org/hibernate/search/v6poc/entity/javabean/JavaBeanMappingContributor.java
@@ -9,16 +9,12 @@ package org.hibernate.search.v6poc.entity.javabean;
 import java.lang.invoke.MethodHandles;
 
 import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
-import org.hibernate.search.v6poc.entity.javabean.mapping.impl.JavaBeanMapperFactory;
-import org.hibernate.search.v6poc.entity.javabean.mapping.impl.JavaBeanMappingImpl;
+import org.hibernate.search.v6poc.entity.javabean.mapping.impl.JavaBeanMappingFactory;
+import org.hibernate.search.v6poc.entity.javabean.mapping.impl.JavaBeanMappingKey;
 import org.hibernate.search.v6poc.entity.javabean.model.impl.JavaBeanBootstrapIntrospector;
 import org.hibernate.search.v6poc.entity.pojo.mapping.spi.PojoMappingContributorImpl;
 
-
-/**
- * @author Yoann Rodiere
- */
-public final class JavaBeanMappingContributor extends PojoMappingContributorImpl<JavaBeanMapping, JavaBeanMappingImpl> {
+public final class JavaBeanMappingContributor extends PojoMappingContributorImpl<JavaBeanMapping> {
 
 	public JavaBeanMappingContributor(SearchMappingRepositoryBuilder mappingRepositoryBuilder) {
 		this( mappingRepositoryBuilder, true );
@@ -36,12 +32,10 @@ public final class JavaBeanMappingContributor extends PojoMappingContributorImpl
 
 	private JavaBeanMappingContributor(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
 			JavaBeanBootstrapIntrospector introspector, boolean annotatedTypeDiscoveryEnabled) {
-		super( mappingRepositoryBuilder, new JavaBeanMapperFactory( introspector ), introspector,
-				annotatedTypeDiscoveryEnabled );
-	}
-
-	@Override
-	protected JavaBeanMapping toReturnType(JavaBeanMappingImpl mapping) {
-		return mapping;
+		super(
+				mappingRepositoryBuilder, new JavaBeanMappingKey(),
+				new JavaBeanMappingFactory(),
+				introspector, annotatedTypeDiscoveryEnabled
+		);
 	}
 }

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/spi/PojoMappingContributorImpl.java
@@ -7,36 +7,33 @@
 package org.hibernate.search.v6poc.entity.pojo.mapping.spi;
 
 import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
-import org.hibernate.search.v6poc.entity.mapping.spi.MappingImplementor;
+import org.hibernate.search.v6poc.entity.mapping.spi.MappingKey;
 import org.hibernate.search.v6poc.entity.pojo.mapping.PojoMapping;
 import org.hibernate.search.v6poc.entity.pojo.mapping.PojoMappingContributor;
-import org.hibernate.search.v6poc.entity.pojo.mapping.building.spi.PojoMapperFactory;
+import org.hibernate.search.v6poc.entity.pojo.mapping.building.impl.PojoMapperFactory;
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.annotation.AnnotationMappingDefinition;
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.annotation.impl.AnnotationMappingDefinitionImpl;
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.programmatic.ProgrammaticMappingDefinition;
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.programmatic.impl.ProgrammaticMappingDefinitionImpl;
 import org.hibernate.search.v6poc.entity.pojo.model.spi.PojoBootstrapIntrospector;
 
-/**
- * @author Yoann Rodiere
- */
-public abstract class PojoMappingContributorImpl<M extends PojoMapping, MI extends MappingImplementor>
+public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 		implements PojoMappingContributor<M> {
 
 	private final SearchMappingRepositoryBuilder mappingRepositoryBuilder;
-
-	private final PojoMapperFactory<MI> mapperFactory;
-
+	private final PojoMapperFactory<M> mapperFactory;
 	private final PojoBootstrapIntrospector introspector;
 
 	private AnnotationMappingDefinitionImpl annotationMappingDefinition;
 
 	protected PojoMappingContributorImpl(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
-			PojoMapperFactory<MI> mapperFactory,
+			MappingKey<M> mappingKey, PojoMappingFactory<M> mappingFactory,
 			PojoBootstrapIntrospector introspector,
 			boolean annotatedTypeDiscoveryEnabled) {
 		this.mappingRepositoryBuilder = mappingRepositoryBuilder;
-		this.mapperFactory = mapperFactory;
+		this.mapperFactory = new PojoMapperFactory<>(
+				mappingKey, mappingFactory, introspector, annotatedTypeDiscoveryEnabled
+		);
 		this.introspector = introspector;
 
 		/*
@@ -54,7 +51,8 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping, MI exten
 
 	@Override
 	public ProgrammaticMappingDefinition programmaticMapping() {
-		ProgrammaticMappingDefinitionImpl definition = new ProgrammaticMappingDefinitionImpl( mapperFactory, introspector );
+		ProgrammaticMappingDefinitionImpl definition =
+				new ProgrammaticMappingDefinitionImpl( mapperFactory, introspector );
 		mappingRepositoryBuilder.addMapping( definition );
 		return definition;
 	}
@@ -66,8 +64,6 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping, MI exten
 
 	@Override
 	public M getResult() {
-		return toReturnType( mappingRepositoryBuilder.getBuiltResult().getMapping( mapperFactory ) );
+		return mappingRepositoryBuilder.getBuiltResult().getMapping( mapperFactory.getMappingKey() );
 	}
-
-	protected abstract M toReturnType(MI mapping);
 }

orm/src/main/java/org/hibernate/search/v6poc/entity/orm/mapping/HibernateOrmMappingContributor.java
@@ -9,14 +9,11 @@ package org.hibernate.search.v6poc.entity.orm.mapping;
 import org.hibernate.boot.Metadata;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
-import org.hibernate.search.v6poc.entity.orm.mapping.impl.HibernateOrmMapperFactory;
-import org.hibernate.search.v6poc.entity.orm.mapping.impl.HibernateOrmMappingImpl;
+import org.hibernate.search.v6poc.entity.orm.mapping.impl.HibernateOrmMappingFactory;
+import org.hibernate.search.v6poc.entity.orm.mapping.impl.HibernateOrmMappingKey;
 import org.hibernate.search.v6poc.entity.orm.model.impl.HibernateOrmBootstrapIntrospector;
 import org.hibernate.search.v6poc.entity.pojo.mapping.spi.PojoMappingContributorImpl;
 
-/**
- * @author Yoann Rodiere
- */
 /*
  * TODO create a Hibernate ORM specific mapper, with the following additions:
  *  1. During mapping creation, use the Hibernate ORM identifier as a fallback when no document ID was found
@@ -26,7 +23,7 @@ import org.hibernate.search.v6poc.entity.pojo.mapping.spi.PojoMappingContributor
  *     when the @DocumentId is NOT the @Id, always ignore the provided ID. See org.hibernate.search.engine.impl.WorkPlan.PerClassWork.extractProperId(Work)
  *  5. And more?
  */
-public class HibernateOrmMappingContributor extends PojoMappingContributorImpl<HibernateOrmMapping, HibernateOrmMappingImpl> {
+public class HibernateOrmMappingContributor extends PojoMappingContributorImpl<HibernateOrmMapping> {
 
 	public HibernateOrmMappingContributor(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
 			Metadata metadata, SessionFactoryImplementor sessionFactoryImplementor,
@@ -38,12 +35,11 @@ public class HibernateOrmMappingContributor extends PojoMappingContributorImpl<H
 	private HibernateOrmMappingContributor(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
 			HibernateOrmBootstrapIntrospector introspector, SessionFactoryImplementor sessionFactoryImplementor,
 			boolean annotatedTypeDiscoveryEnabled) {
-		super( mappingRepositoryBuilder, new HibernateOrmMapperFactory( introspector, sessionFactoryImplementor ),
-				introspector, annotatedTypeDiscoveryEnabled );
+		super(
+				mappingRepositoryBuilder, new HibernateOrmMappingKey(),
+				new HibernateOrmMappingFactory( sessionFactoryImplementor ),
+				introspector, annotatedTypeDiscoveryEnabled
+		);
 	}
 
-	@Override
-	protected HibernateOrmMapping toReturnType(HibernateOrmMappingImpl mapping) {
-		return mapping;
-	}
 }

engine/src/main/java/org/hibernate/search/v6poc/engine/SearchMappingRepository.java
@@ -10,9 +10,6 @@ import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.impl.SearchMappingRepositoryBuilderImpl;
 import org.hibernate.search.v6poc.entity.mapping.spi.MappingKey;
 
-/**
- * @author Yoann Rodiere
- */
 public interface SearchMappingRepository extends AutoCloseable {
 
 	<M> M getMapping(MappingKey<M> mappingKey);

engine/src/main/java/org/hibernate/search/v6poc/engine/impl/SearchMappingRepositoryBuilderImpl.java
@@ -40,10 +40,6 @@ import org.hibernate.search.v6poc.entity.model.spi.MappableTypeModel;
 import org.hibernate.search.v6poc.util.AssertionFailure;
 import org.hibernate.search.v6poc.util.SearchException;
 
-
-/**
- * @author Yoann Rodiere
- */
 public class SearchMappingRepositoryBuilderImpl implements SearchMappingRepositoryBuilder {
 
 	private final ConfigurationPropertySource mainPropertySource;
@@ -105,10 +101,10 @@ public class SearchMappingRepositoryBuilderImpl implements SearchMappingReposito
 		Map<MappingKey<?>, Mapper<?, ?>> mappers =
 				metadataCollector.createMappers( buildContext, propertySource, indexManagerBuildingStateProvider );
 
-		Map<MappingKey<?>, MappingImplementor> mappings = new HashMap<>();
+		Map<MappingKey<?>, MappingImplementor<?>> mappings = new HashMap<>();
 		// TODO close the mappings created so far if anything fails after this
 		mappers.forEach( (mappingKey, mapper) -> {
-			MappingImplementor mapping = mapper.build();
+			MappingImplementor<?> mapping = mapper.build();
 			mappings.put( mappingKey, mapping );
 		} );
 
@@ -163,7 +159,7 @@ public class SearchMappingRepositoryBuilderImpl implements SearchMappingReposito
 		private <C> MapperContribution<C, ?> getOrCreateContribution(
 				MapperFactory<C, ?> mapperFactory) {
 			return (MapperContribution<C, ?>) contributionByMappingKey.computeIfAbsent(
-					mapperFactory, ignored -> new MapperContribution<>( mapperFactory )
+					mapperFactory.getMappingKey(), ignored -> new MapperContribution<>( mapperFactory )
 			);
 		}
 
@@ -183,7 +179,7 @@ public class SearchMappingRepositoryBuilderImpl implements SearchMappingReposito
 		}
 	}
 
-	private static class MapperContribution<C, M extends MappingImplementor> {
+	private static class MapperContribution<C, M> {
 
 		private final MapperFactory<C, M> mapperFactory;
 		private final Map<MappableTypeModel, TypeMappingContribution<C>> contributionByType = new LinkedHashMap<>();

engine/src/main/java/org/hibernate/search/v6poc/engine/impl/SearchMappingRepositoryImpl.java
@@ -15,16 +15,12 @@ import org.hibernate.search.v6poc.entity.mapping.spi.MappingImplementor;
 import org.hibernate.search.v6poc.util.SearchException;
 import org.hibernate.search.v6poc.util.spi.Closer;
 
-
-/**
- * @author Yoann Rodiere
- */
 public class SearchMappingRepositoryImpl implements SearchMappingRepository {
 
-	private final Map<MappingKey<?>, MappingImplementor> mappings;
+	private final Map<MappingKey<?>, MappingImplementor<?>> mappings;
 	private final Map<String, Backend<?>> backends;
 
-	SearchMappingRepositoryImpl(Map<MappingKey<?>, MappingImplementor> mappings, Map<String, Backend<?>> backends) {
+	SearchMappingRepositoryImpl(Map<MappingKey<?>, MappingImplementor<?>> mappings, Map<String, Backend<?>> backends) {
 		this.mappings = mappings;
 		this.backends = backends;
 	}
@@ -32,7 +28,7 @@ public class SearchMappingRepositoryImpl implements SearchMappingRepository {
 	@Override
 	public <M> M getMapping(MappingKey<M> mappingKey) {
 		@SuppressWarnings("unchecked") // See SearchMappingRepositoryBuilderImpl: we are sure that, if there is a mapping, it implements M
-		M mapping = (M) mappings.get( mappingKey );
+		M mapping = (M) mappings.get( mappingKey ).toAPI();
 		if ( mapping == null ) {
 			throw new SearchException( "No mapping registered for mapping key '" + mappingKey + "'" );
 		}

engine/src/main/java/org/hibernate/search/v6poc/entity/mapping/building/spi/Mapper.java
@@ -9,10 +9,7 @@ package org.hibernate.search.v6poc.entity.mapping.building.spi;
 import org.hibernate.search.v6poc.entity.mapping.spi.MappingImplementor;
 import org.hibernate.search.v6poc.entity.model.spi.MappableTypeModel;
 
-/**
- * @author Yoann Rodiere
- */
-public interface Mapper<C, M extends MappingImplementor> {
+public interface Mapper<C, M> {
 
 	/**
 	 * @param typeModel A model of the type to be mapped
@@ -24,6 +21,6 @@ public interface Mapper<C, M extends MappingImplementor> {
 			IndexManagerBuildingState<?> indexManagerBuildingState,
 			TypeMetadataContributorProvider<C> contributorProvider);
 
-	M build();
+	MappingImplementor<M> build();
 
 }

engine/src/main/java/org/hibernate/search/v6poc/entity/mapping/building/spi/MapperFactory.java
@@ -8,14 +8,11 @@ package org.hibernate.search.v6poc.entity.mapping.building.spi;
 
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
-import org.hibernate.search.v6poc.entity.mapping.spi.MappingImplementor;
 import org.hibernate.search.v6poc.entity.mapping.spi.MappingKey;
 
-/**
- * @author Yoann Rodiere
- */
-public interface MapperFactory<C, M extends MappingImplementor>
-		extends MappingKey<M> {
+public interface MapperFactory<C, M> {
+
+	MappingKey<M> getMappingKey();
 
 	Mapper<C, M> createMapper(BuildContext buildContext, ConfigurationPropertySource propertySource);
 

commit a4f8a0f11894443775a5ab37b370146c7a0998ab

Restore the implicitProvidedId setting from before the PojoMapperFactory changes
The bug was introduced in 70aadce41b6d67f6934f05066f2eef0272ed6e06

mapper-javabean/src/main/java/org/hibernate/search/v6poc/entity/javabean/JavaBeanMappingContributor.java
@@ -35,7 +35,8 @@ public final class JavaBeanMappingContributor extends PojoMappingContributorImpl
 		super(
 				mappingRepositoryBuilder, new JavaBeanMappingKey(),
 				new JavaBeanMappingFactory(),
-				introspector, annotatedTypeDiscoveryEnabled
+				introspector, false,
+				annotatedTypeDiscoveryEnabled
 		);
 	}
 }

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/spi/PojoMappingContributorImpl.java
@@ -29,10 +29,11 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 	protected PojoMappingContributorImpl(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
 			MappingKey<M> mappingKey, PojoMappingFactory<M> mappingFactory,
 			PojoBootstrapIntrospector introspector,
+			boolean implicitProvidedId,
 			boolean annotatedTypeDiscoveryEnabled) {
 		this.mappingRepositoryBuilder = mappingRepositoryBuilder;
 		this.mapperFactory = new PojoMapperFactory<>(
-				mappingKey, mappingFactory, introspector, annotatedTypeDiscoveryEnabled
+				mappingKey, mappingFactory, introspector, implicitProvidedId
 		);
 		this.introspector = introspector;
 

orm/src/main/java/org/hibernate/search/v6poc/entity/orm/mapping/HibernateOrmMappingContributor.java
@@ -38,7 +38,8 @@ public class HibernateOrmMappingContributor extends PojoMappingContributorImpl<H
 		super(
 				mappingRepositoryBuilder, new HibernateOrmMappingKey(),
 				new HibernateOrmMappingFactory( sessionFactoryImplementor ),
-				introspector, annotatedTypeDiscoveryEnabled
+				introspector, false,
+				annotatedTypeDiscoveryEnabled
 		);
 	}
 