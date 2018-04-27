915801ee75aaf51208d1246c1562d64a1aba8226
Guillaume Smet <guillaume.smet@gmail.com>
2018-04-27 11:44:13 +0200

    HSEARCH-3094 Throw exceptions for match/range queries on GeoPoint
    Note that, in the case of Lucene, we could support exact match queries
    by using a PointRangeQuery (see LatLonPoint for how to use it) but, for
    the time being, we chose to be consistent with Elasticsearch.

---
+++ backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/predicate/impl/GeoPointFieldPredicateBuilderFactory.java
@@ -0,0 +1,35 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.predicate.impl;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
+import org.hibernate.search.v6poc.backend.lucene.search.predicate.impl.LuceneSearchPredicateCollector;
+import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
+
+public final class GeoPointFieldPredicateBuilderFactory implements LuceneFieldPredicateBuilderFactory {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	public static final GeoPointFieldPredicateBuilderFactory INSTANCE = new GeoPointFieldPredicateBuilderFactory();
+
+	private GeoPointFieldPredicateBuilderFactory() {
+	}
+
+	@Override
+	public MatchPredicateBuilder<LuceneSearchPredicateCollector> createMatchPredicateBuilder(String absoluteFieldPath) {
+		throw log.matchPredicatesNotSupportedByGeoPoint( absoluteFieldPath );
+	}
+
+	@Override
+	public RangePredicateBuilder<LuceneSearchPredicateCollector> createRangePredicateBuilder(String absoluteFieldPath) {
+		throw log.rangePredicatesNotSupportedByGeoPoint( absoluteFieldPath );
+	}
+}
