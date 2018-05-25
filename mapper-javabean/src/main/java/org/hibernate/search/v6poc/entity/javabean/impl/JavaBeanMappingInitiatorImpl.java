f24477b88fc8598caf72354d18e618bb393ab3ff
Yoann Rodière <yoann@hibernate.org>
2018-05-25 10:25:01 +0200

    HSEARCH-3177 Do not expose PojoMappingInitiatorImpl through the JavaBean mapper APIs
    PojoMappingInitiatorImpl is an SPI type, which in particular exposes
    implementations of MappingInitiator methods that we don't want users to
    see.

---
+++ mapper-javabean/src/main/java/org/hibernate/search/v6poc/entity/javabean/impl/JavaBeanMappingInitiatorImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.entity.javabean.impl;
+
+import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
+import org.hibernate.search.v6poc.entity.javabean.JavaBeanMapping;
+import org.hibernate.search.v6poc.entity.javabean.JavaBeanMappingInitiator;
+import org.hibernate.search.v6poc.entity.javabean.mapping.impl.JavaBeanMappingFactory;
+import org.hibernate.search.v6poc.entity.javabean.mapping.impl.JavaBeanMappingKey;
+import org.hibernate.search.v6poc.entity.javabean.model.impl.JavaBeanBootstrapIntrospector;
+import org.hibernate.search.v6poc.entity.pojo.mapping.spi.PojoMappingInitiatorImpl;
+
+public final class JavaBeanMappingInitiatorImpl extends PojoMappingInitiatorImpl<JavaBeanMapping>
+		implements JavaBeanMappingInitiator {
+
+	public JavaBeanMappingInitiatorImpl(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
+			JavaBeanBootstrapIntrospector introspector,
+			boolean annotatedTypeDiscoveryEnabled, boolean multiTenancyEnabled) {
+		super(
+				mappingRepositoryBuilder, new JavaBeanMappingKey(),
+				new JavaBeanMappingFactory(),
+				introspector, false,
+				annotatedTypeDiscoveryEnabled, multiTenancyEnabled
+		);
+	}
+
+}
