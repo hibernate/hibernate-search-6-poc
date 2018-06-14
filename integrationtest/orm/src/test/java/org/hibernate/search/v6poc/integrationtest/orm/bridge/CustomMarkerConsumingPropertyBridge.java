efd0ea3818a673f44b779d25f2c96c0cebeb7339
Yoann Rodière <yoann@hibernate.org>
2018-06-14 14:53:32 +0200

    HSEARCH-3205 Allow to map annotations to a bridge directly, without a builder
    This makes sense when the annotation doesn't take any parameter, which
    should be the case for the simplest bridges.

--- integrationtest/orm/src/test/java/org/hibernate/search/v6poc/integrationtest/orm/bridge/CustomMarkerConsumingPropertyBridge.java
+++ integrationtest/orm/src/test/java/org/hibernate/search/v6poc/integrationtest/orm/bridge/CustomMarkerConsumingPropertyBridge.java
@@ -13,33 +13,14 @@ import java.util.stream.Collectors;
 import org.hibernate.search.v6poc.backend.document.DocumentElement;
 import org.hibernate.search.v6poc.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
-import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.model.SearchModel;
 import org.hibernate.search.v6poc.entity.pojo.bridge.PropertyBridge;
-import org.hibernate.search.v6poc.entity.pojo.bridge.mapping.AnnotationBridgeBuilder;
 import org.hibernate.search.v6poc.entity.pojo.model.PojoElement;
 import org.hibernate.search.v6poc.entity.pojo.model.PojoModelProperty;
-import org.hibernate.search.v6poc.integrationtest.orm.bridge.annotation.CustomMarkerConsumingPropertyBridgeAnnotation;
 
 public final class CustomMarkerConsumingPropertyBridge implements PropertyBridge {
 
-	public static final class Builder
-			implements AnnotationBridgeBuilder<PropertyBridge, CustomMarkerConsumingPropertyBridgeAnnotation> {
-		@Override
-		public void initialize(CustomMarkerConsumingPropertyBridgeAnnotation annotation) {
-			// Nothing to do
-		}
-
-		@Override
-		public PropertyBridge build(BuildContext buildContext) {
-			return new CustomMarkerConsumingPropertyBridge();
-		}
-	}
-
 	private List<IndexObjectFieldAccessor> objectFieldAccessors = new ArrayList<>();
-
-	private CustomMarkerConsumingPropertyBridge() {
-	}
 
 	@Override
 	public void bind(IndexSchemaElement indexSchemaElement, PojoModelProperty bridgedPojoModelProperty,
